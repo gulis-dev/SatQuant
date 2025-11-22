a6ddf37811e7b6a6dff132b0e568ebbc789d3b06
gulis-dev <gulispolska@gmail.com>
2025-11-22 16:21:14 +0100

    Initial project setup and dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='satquant',
+    version='0.1.0',
+    description='Satellite Imagery Quantization Library with Focus Calibration',
+    author='Oskar Andrukiewicz',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+        'numpy',
+        'opencv-python'
+    ],
+)
